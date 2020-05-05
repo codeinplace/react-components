0e28ba17ffc151a67af289980ec0a641a3a33e0d
Jonathan Rocha <johnferreirar@gmail.com>
2020-05-04 22:28:50 -0300

    feat: config project for examples with local package on develop

---
+++ example/src/app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Teste } from '@codeinplace/react-components';
+
+const App = () => {
+    return (
+        <div>
+            My App: <Teste />
+        </div>
+    );
+};
+
+export default App;
